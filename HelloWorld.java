04e6bb47075d686e8edfb05f118ec1f1361ad2bf
vmicuk <vitalij.micuk@gmail.com>
2017-03-13 17:42:46 +0200

    Modified HelloWorld.java, add today date

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@
  * @author zwaclaw
  *
  */
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class HelloWorld {
 	public static void main(String[] args) {
@@ -9,6 +12,9 @@ public class HelloWorld {
 	System.out.println("The World is not ugly!");
 	Input in = new Input();
 	in.insertName();
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	Date date = new Date();
+	System.out.println("Today: " + dateFormat.format(date));
 	System.out.println("Hello " + in.getName());
 }
 }
