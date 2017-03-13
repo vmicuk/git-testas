bdcfa7f451b9797f0df8b5bc45e5c28a7968ab08
vmicuk <vitalij.micuk@gmail.com>
2017-03-13 17:27:16 +0200

    Modified HelloWorld.java to use Input.java, create method to print Hello name

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!");
 	System.out.println("The World is not ugly!");
+	Input in = new Input();
+	in.insertName();
+	System.out.println("Hello " + in.getName());
 }
 }
