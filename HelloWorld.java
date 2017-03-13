ce0f59353283f875f6c86a41884ac79649ff7346
vmicuk <vitalij.micuk@gmail.com>
2017-03-13 17:50:32 +0200

    Modified HelloWorld.java, add real date

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,19 @@
  *
  */
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HELLO UGLY WORLD!");
+	System.out.println("HELLO UGLY WORLD!");
 	System.out.println("The World is not ugly!");
+	Input in = new Input();
+	in.insertName();
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	Date date = new Date();
+	System.out.println("Today: " + dateFormat.format(date));
+	System.out.println("Hello " + in.getName());
 }
 }
